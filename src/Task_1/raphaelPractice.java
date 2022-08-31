f9fdd07c5525ee69abab790c3e330dc64d62139c
raphaelnyangenya <raphael@esusu.org>
2022-09-01 01:01:53 +0300

    created raphael Branch

---
+++ src/Task_1/raphaelPractice.java
@@ -0,0 +1,12 @@
+package Task_1;
+
+public class raphaelPractice {
+
+
+    public static void main(String[] args) {
+        int y = 6;
+        int t =7 ;
+
+        // this is my practice
+    }
+}
