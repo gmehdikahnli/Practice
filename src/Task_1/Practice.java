e5f55650ea2a2a5ba3a2aecc938be8e82b2b4470
Sergei Khvalev <skhvalev83@gmail.com>
2022-08-31 18:02:07 -0400

    first GitHub practice from Sergei

---
+++ src/Task_1/Practice.java
@@ -0,0 +1,10 @@
+package Task_1;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        int e = 17;
+        double v = 2.3;
+        // this is my simple code
+    }
+}
