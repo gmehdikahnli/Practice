bf3cbcbb1c492b4e0018a452171f40a2526aba73
Nihat Asgarov <asgarovnihat@gmail.com>
2022-08-31 18:23:17 -0400

    Nihats updated 2

--- src/Task_1/NihatPractice.java
+++ src/Task_1/NihatPractice.java
@@ -4,5 +4,8 @@ public class NihatPractice {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello guys");
+
+        int a = 5;
+        int b = 10;
     }
 }
